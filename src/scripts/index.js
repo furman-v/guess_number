0284b7adf65b24e59a84cdd94fd4ee29f63a5f56
furman-v <victoria.furman5@gmail.com>
2020-03-27 19:12:09 +0200

    guess_the_number

---
+++ src/scripts/index.js
@@ -0,0 +1,36 @@
+import "../styles/index.scss";
+let randomNumber;
+function init() {
+  randomNumber = getRandomNumber(9);
+  const input = document.getElementById("checkInput");
+  const button = document.getElementById("checkBtn");
+  button.addEventListener("click", () => {
+    checkNumber(input, randomNumber);
+  });
+}
+init();
+
+function checkNumber(input, number) {
+  if (input.value == "") {
+    alert("Please add number");
+  } else {
+    checkValue(input, number);
+  }
+  function checkValue(input, number) {
+    const prgrph = document.getElementById("paragraph");
+    if (input.value > number) {
+      prgrph.innerHTML = "Your number is more than allowed";
+      prgrph.className = "red";
+    } else if (input.value < number) {
+      prgrph.innerHTML = "Your number is less than Random Number";
+      prgrph.className = "green";
+    } else {
+      prgrph.innerHTML = `You won! I was Thinking about ${number}`;
+      prgrph.className = "yellow";
+      randomNumber = getRandomNumber(8);
+    }
+  }
+}
+function getRandomNumber(n) {
+  return Math.floor(Math.random() * n + 1);
+}
